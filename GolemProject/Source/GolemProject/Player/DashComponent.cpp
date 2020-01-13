64d6d9e63b8ece0bce86fa5bf4bd855ce790006f
SynysterRev <michael.arn@live.fr>
2020-01-13 17:55:13 +0100

    dashComponent

---
+++ GolemProject/Source/GolemProject/Player/DashComponent.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DashComponent.h"
+#include "GolemProjectCharacter.h"
+
+// Sets default values for this component's properties
+UDashComponent::UDashComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UDashComponent::BeginPlay()
+{
+	Super::BeginPlay();	
+	m_character = Cast<AGolemProjectCharacter>(GetOwner());
+	// ...
+	
+}
+
+
+
+
+// Called every frame
+void UDashComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	// ...
+}
+
+void UDashComponent::Dash()
+{
+	if (m_character)
+	{
+		m_character->LaunchCharacter(velocityDash, true, true);
+	}
+}
+
