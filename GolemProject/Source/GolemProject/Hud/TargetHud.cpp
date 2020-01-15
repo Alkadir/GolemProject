b15a1096fa875b1d7af6e91b62f328113126e53d
imegard <robbordel@gmail.com>
2020-01-15 12:15:28 +0100

    try to add sight HUD

---
+++ GolemProject/Source/GolemProject/Hud/TargetHud.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TargetHud.h"
+#include "Engine/World.h"
+#include "Blueprint/UserWidget.h"
+
+void UTargetHud::BeginPlay()
+{
+	/*UUserWidget* mWidgetSight = CreateWidget<UUserWidget>(this, widgetSightClass, "target");
+	mWidgetSight->AddToViewport();*/
+}
---
+++ GolemProject/Source/GolemProject/Hud/TargetHud.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "TargetHud.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOLEMPROJECT_API UTargetHud : public UWidgetComponent
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = " Hud Sight")
+	TSubclassOf<class UUserWidget> widgetSightClass;
+
+	protected:
+	class UUserWidget* mWidgetSight;
+	virtual void BeginPlay() override;
+};
