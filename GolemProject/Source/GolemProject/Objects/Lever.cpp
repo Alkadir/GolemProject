ebd2fc5bc491cc1552da6d6cd2f69e176190730a
Alkadir <alkadir84@hotmail.fr>
2020-03-02 15:28:35 +0100

    Lot of minor changes
    change debugScreen by Helper::Print
    print is now unavailable on build
    fix on projectile collision with destructible
    add more feedbacks for LevelDesigner on objects
    more security on pointer

--- GolemProject/Source/GolemProject/Objects/Lever.cpp
+++ GolemProject/Source/GolemProject/Objects/Lever.cpp
@@ -2,7 +2,7 @@
 
 
 #include "Lever.h"
-#include "Engine/Engine.h"
+#include "Helpers/HelperLibrary.h"
 #include "Components/StaticMeshComponent.h"
 #include "GolemProjectCharacter.h"
 
@@ -14,33 +14,52 @@ ALever::ALever()
 
 }
 
+void ALever::BeginPlay()
+{
+	Super::BeginPlay();
+	if (objectsToActivate.Num() == 0)
+	{
+		HelperLibrary::Print(FString::Printf(TEXT("WARNING %s don\'t have objects to interact"), *(GetName())), 5.f, FColor::Yellow);
+	}
+	for (auto& objectToActivate : objectsToActivate)
+	{
+		if (objectToActivate == nullptr)
+		{
+			HelperLibrary::Print(FString::Printf(TEXT("WARNING %s have none value on object to activate"), *(GetName())), 5.f, FColor::Yellow);
+		}
+	}
+}
+
 const bool ALever::Interact_Implementation(AActor* caller)
 {
 	bool haveActivate = false;
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("Interact")));
+	HelperLibrary::Print(FString::Printf(TEXT("Interact with %s"), *(GetName())), 5.f, FColor::Green);
 	for (auto& objectToActivate : objectsToActivate)
 	{
-		if (IActivable * activable = Cast<IActivable>(objectToActivate))
+		if (objectToActivate != nullptr)
 		{
-			if (activationType == EActivationType::Activate)
+			if (IActivable * activable = Cast<IActivable>(objectToActivate))
 			{
-				activable->Execute_Activate(objectToActivate, this);
-				haveActivate = true;
+				if (activationType == EActivationType::Activate)
+				{
+					activable->Execute_Activate(objectToActivate, this);
+					haveActivate = true;
+				}
+				else if (activationType == EActivationType::Desactivate)
+				{
+					activable->Execute_Desactivate(objectToActivate, this);
+					haveActivate = true;
+				}
+				else if (activationType == EActivationType::Switch)
+				{
+					activable->Execute_Switch(objectToActivate, this);
+					haveActivate = true;
+				}
 			}
-			else if (activationType == EActivationType::Desactivate)
+			else
 			{
-				activable->Execute_Desactivate(objectToActivate, this);
-				haveActivate = true;
+				HelperLibrary::Print(FString::Printf(TEXT("%s don\'t implement Activable interface"), *(objectToActivate->GetName())), 5.f, FColor::Yellow);
 			}
-			else if (activationType == EActivationType::Switch)
-			{
-				activable->Execute_Switch(objectToActivate, this);
-				haveActivate = true;
-			}
-		}
-		else
-		{
-			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT("%s don\'t implement Activable interface"), *(objectToActivate->GetName())));
 		}
 	}
 	return haveActivate;
