f60c621ce1af89608ccb9b34fce707f13229d371
Louis Siani <l.siani92@gmail.com>
2020-04-30 23:30:57 +0200

    recreate lvl2

--- GolemProject/Source/GolemProject/Objects/Door.h
+++ GolemProject/Source/GolemProject/Objects/Door.h
@@ -27,7 +27,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditAnywhere)
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
 		EOpenDoorBy openBy;
 
 	UPROPERTY(EditDefaultsOnly)
